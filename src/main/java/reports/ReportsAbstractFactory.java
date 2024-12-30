e786529f04dbb82d12f567420530beddb8359355
Dinesh Kumar Loganathan <dineshkumar@squareup.com>
2024-12-30 13:15:06 +0100

    remove unused code

--- src/main/java/reports/ReportsAbstractFactory.java
+++ src/main/java/reports/ReportsAbstractFactory.java
@@ -6,5 +6,4 @@ import reports.revenue_reports.RevenueReportGenerator;
 public abstract class ReportsAbstractFactory {
     public abstract ProviderReportGenerator generateMarketShare(String provider);
     public abstract RevenueReportGenerator generateRevenue(String provider);
-    public abstract RevenueReportGenerator generateDummyReport(String provider);
 }
